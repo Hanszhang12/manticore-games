26a95efff7c0a970eec4640b9a0564d909865b7d
Hans Zhang <>
2021-04-27 11:59:29 -0700

    fixed

--- youtube_scraper.py
+++ youtube_scraper.py
@@ -1,12 +1,25 @@
 import requests
+import pandas as pd 
+import numpy as np 
 from bs4 import BeautifulSoup
 
+##function takes in array of youtube links
 def youtubeViews(input):
 	videos = input
 	views = []
 	for video in videos:
-		url = video
-		soup = BeautifulSoup(requests.get(url).text, 'lxml')
-		views.append(soup.select_one('meta[itemprop = "interactionCount"][content]')['content'])
+		if type(video) == str:
+			video = video.split("\n")
+			for url in video:
+				url = url.split(" ")
+				for link in url:
+					if link[0:5] == 'https':
+						soup = BeautifulSoup(requests.get(link).text, 'lxml')
+						views.append(soup.find(itemprop="interactionCount")['content'])
+	return views
 
-	##put links/views into CSV 
+##test
+monthlyResponses = "Core_Affiliate_February.csv"
+monthlyResponsesDf = pd.read_csv("inputs/" + monthlyResponses)
+x = monthlyResponsesDf["If your primary platform is YouTube, please link all new Core videos for the month of February below. "]
+print(youtubeViews(x))
