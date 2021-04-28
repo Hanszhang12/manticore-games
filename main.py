71423bb22d8baa5b7aecc0493b71ccc1a678cbd4
Jeffrey Yao <jfyao1454@gmail.com>
2021-04-29 01:47:34 +0800

    changes to sullygnome scraper

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ def youtube_Tier(views) :
             tiers.append("--")
             cash.append(0)
     return tiers, cash
-# twitchDf = finalDf[finalDf['Team Name'] == 'Team Twitch']
-# twitchDf['Average Views'] = sullygnome_webscraper.scrape_list(twitchDf['Stats Link'])
+twitchDf = finalDf[finalDf['Team Name'] == 'Team Twitch']
+twitchDf['Monthly Average Viewers'], twitchDf['Monthly Hours']= sullygnome_webscraper.scrape_list(twitchDf['Stats Link'])
 
-youtubeDf = finalDf[finalDf['Team Name'] == 'Team YouTube']
-youtubeDf['Monthly Videos'], youtubeDf['Monthly Views'] = youtube_scraper.youtubeViews(youtubeDf['Links'])
-youtubeDf['Tier'], youtubeDf['Cash Reward'] = youtube_Tier(youtubeDf['Monthly Views'])
-youtubeDf.rename(columns={"Account Creates":"Referral Signups", "Account Verifies":"Referral Account Creation"}, inplace=True)
-youtubeDf = youtubeDf[['Name', 'Core Username',	'Email Address', 'Tier', 'Cash Reward',	'Monthly Videos', 'Monthly Views', 'Stats Link', 'Referral Clicks', 'Referral Signups', 'Referral Account Creation']]
-youtubeDf['Total Cash Reward'] = youtubeDf['Cash Reward'] + 2 * youtubeDf['Referral Account Creation']
-youtubeDf['Account Creation Percentage'] = youtubeDf['Referral Clicks'] / youtubeDf['Referral Signups']
-youtubeDf['Verification Percentage'] = youtubeDf['Referral Signups'] / youtubeDf['Referral Account Creation']
-youtubeDf.to_csv("outputs/youtube_affiliate_data.csv")
+# youtubeDf = finalDf[finalDf['Team Name'] == 'Team YouTube']
+# youtubeDf['Monthly Videos'], youtubeDf['Monthly Views'] = youtube_scraper.youtubeViews(youtubeDf['Links'])
+# youtubeDf['Tier'], youtubeDf['Cash Reward'] = youtube_Tier(youtubeDf['Monthly Views'])
+# youtubeDf.rename(columns={"Account Creates":"Referral Signups", "Account Verifies":"Referral Account Creation"}, inplace=True)
+# youtubeDf = youtubeDf[['Name', 'Core Username',	'Email Address', 'Tier', 'Cash Reward',	'Monthly Videos', 'Monthly Views', 'Stats Link', 'Referral Clicks', 'Referral Signups', 'Referral Account Creation']]
+# youtubeDf['Total Cash Reward'] = youtubeDf['Cash Reward'] + 2 * youtubeDf['Referral Account Creation']
+# youtubeDf['Account Creation Percentage'] = youtubeDf['Referral Clicks'] / youtubeDf['Referral Signups']
+# youtubeDf['Verification Percentage'] = youtubeDf['Referral Signups'] / youtubeDf['Referral Account Creation']
+# youtubeDf.to_csv("outputs/youtube_affiliate_data.csv")
